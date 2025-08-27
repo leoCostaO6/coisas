47e87750a8d72563af7201c678eeffdc9b5ec2ab
leoCostaO6 <leo.costa.mendonca1467@gmail.com>
2025-08-26 23:00:11 -0400

    Update naoSei.js

--- naoSei.js
+++ naoSei.js
@@ -1,10 +1,6 @@
 const btEnvio = document.getElementById('btEnvio');
 const novoElemento = document.getElementById('novoElemento');
 
- var nome = document.getElementById('nome').ariaValueText;
- var idade = document.getElementById('idade').valu;
- var filmePref = document.getElementById('filmePref');
- var jogoPref = document.getElementById('jogoPref');
     
 
 function addNovoElemento(){
